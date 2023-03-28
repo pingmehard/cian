53f6e7d684213d0a3dbb3d554c03e6560933f0f7
Roman Polyansky <roman.polyansky@email.com>
2023-03-28 23:56:49 +0300

    added threads, show raw flats

--- bot.py
+++ bot.py
@@ -1,9 +1,55 @@
+import datetime
+import time
+import pickle
+from threading import Thread
+
 import telebot
 
 from creds import token
+from main import proceed_flats
+
+dict_convert = {
+        0 : "remont edition",
+        1 : "neponal nah",
+        2 : "babka edition",
+        3 : "ebat` berem"
+    }
+
+cian_link = "https://www.cian.ru/"
 
 bot = telebot.TeleBot(token)
 
+
+
 def send_message_bot(text):
 
-    bot.send_message(chat_id = "@cnews_channel", text = text)
+    bot.send_message(chat_id = "@cian_news", text = text)
+
+@bot.message_handler(commands=["show_raw"])
+def show_raw_flats(message):
+
+    with open('./data/offers.pickle', 'rb') as f:
+            # dump the data to the file
+            offers = pickle.load(f)
+
+    filtered_offers = filter(lambda x: datetime.datetime.today().strftime('%Y-%m-%d') == x['LoadDate'], offers)
+
+    for text in [cian_link + i['Link'] + '\n' for i in filtered_offers if i['Result'] == dict_convert[0]]:
+
+        try:
+            res = send_message_bot(text)
+        except:
+            time.sleep(60)
+
+        time.sleep(.5)
+
+def sheduler():
+    while True: 
+        proceed_flats()
+        time.sleep(24 * 60 * 60)
+
+thread1 = Thread(target=bot.infinity_polling)
+thread2 = Thread(target=sheduler)
+
+thread1.start()
+thread2.start()
