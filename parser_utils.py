c7ce6965d0d67055b827bbc1d25bdac9b80ccb7b
rpolyanskiy <rpolyanskiy@solit-clouds.ru>
2023-04-29 20:40:30 +0300

    added first creation date for flat

--- parser_utils.py
+++ parser_utils.py
@@ -1,9 +1,51 @@
 import time
+import datetime
 
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 
 from bs4 import BeautifulSoup as BS
+
+
+month_map = {
+    "янв" : "01",
+    "фев" : "02",
+    "мар" : "03",
+    "апр" : "04",
+    "май" : "05",
+    "июн" : "06",
+    "июл" : "07",
+    "авг" : "08",
+    "сен" : "09",
+    "окт" : "10",
+    "ноя" : "11",
+    "дек" : "12",
+}
+
+
+
+def get_offer_create_date(driver, offer_link):
+
+    # откарываем карточку квартиры
+    driver.get(offer_link)
+    page_feed = BS(driver.page_source, 'lxml')
+    # Находим класс всплывашки история цены, затем находим все указанные в ней цены и берем последнюю, так как она и самая первая цена
+    first_price = page_feed.find('div', class_ = "a10a3f92e9--container--y2J5W").find_all('tr', class_ = "a10a3f92e9--history-event--xUQ_P")[-1]
+    # получаем первую дату из текста
+    first_date = first_price.find('td', class_ = "a10a3f92e9--event-date--BvijC").text
+
+    splitted_first_date = first_date.split()
+    try:
+        day = int(splitted_first_date[0])
+        month = int(month_map[splitted_first_date[1]])
+        year = int(splitted_first_date[2])
+    except:
+        return ''
+
+    # first_date_pythonic = datetime.date(year, month, day)
+    # return first_date_pythonic
+
+    return ".".join([year, month, day])
 
 
 def get_flat_info(page_feed, offers_load_status, offer_links):
@@ -29,6 +71,7 @@ def get_flat_info(page_feed, offers_load_status, offer_links):
         offer['Link'] = flat.find('a')['href']
         offer['Price'] = flat.find('span', attrs={"data-mark":"MainPrice"}).text
         offer['Images'] = [i['src'] for i in flat.find_all('img') if ".jpg" in i['src']]
+        offer['FirstHistoryDate'] = get_offer_create_date(offer['Link'])
 
         offers += [offer]
         
